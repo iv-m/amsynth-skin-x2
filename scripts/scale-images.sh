09040e9793614791681fdc16a2403fb3e8aa0e92
Ivan A. Melnikov <iv@altlinux.org>
2023-01-05 12:40:47 +0400

    Resize differently
    After some experimentation, this strategy turned out
    to be the best one for 2x resizing.

--- scripts/scale-images.sh
+++ scripts/scale-images.sh
@@ -4,6 +4,15 @@ set -xeu
 
 cd "$1"
 
+
+# other possible strategies:
+# * -resize '200%'
+# * -resize '200%' -unsharp 0x6+0.5+0
+# * -adaptive-resize '200%'
+
 for img in *.png; do
-    convert "$img" -resize '200%' "$img"
+    convert "$img" \
+        -interpolate Nearest \
+        -interpolative-resize '200%' \
+        "$img"
 done
